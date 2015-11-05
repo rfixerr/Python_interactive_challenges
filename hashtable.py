4613540443871634be3be9bb31c39b6044de7cb2
Radu Bogdan <bogdandradu@yahoo.com>
2015-11-05 17:59:49 +0200

    Initial commit

---
+++ hashtable.py
@@ -0,0 +1,46 @@
+'''Implement a hash table with set, get, and remove methods.
+    Constraints
+
+    For simplicity, are the keys integers only? Yes
+    For collision resolution, can we use chaining? Yes
+    Do we have to worry about load factors? No
+
+    Test Cases
+
+    get on an empty hash table index
+    set on an empty hash table index
+    set on a non empty hash table index
+    set on a key that already exists
+    remove on a key with an entry
+    remove on a key without an entry'''
+
+
+
+class Item(object):
+
+    def __init__(self, key, value):
+        # TODO: Implement me
+        pass
+
+
+class HashTable(object):
+
+    def __init__(self, size):
+        # TODO: Implement me
+        pass
+
+    def hash_function(self, key):
+        # TODO: Implement me
+        pass
+
+    def set(self, key, value):
+        # TODO: Implement me
+        pass
+
+    def get(self, key):
+        # TODO: Implement me
+        pass
+
+    def remove(self, key):
+        # TODO: Implement me
+        pass
